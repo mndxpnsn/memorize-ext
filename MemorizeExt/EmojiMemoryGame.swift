f57266c97349036e42155a13dbf53730fd90e7d3
derek <harrison.derek.w@gmail.com>
2021-12-09 17:58:02 -0400

    add startui

--- MemorizeExt/EmojiMemoryGame.swift
+++ MemorizeExt/EmojiMemoryGame.swift
@@ -8,13 +8,19 @@
 //import Foundation
 import SwiftUI
 
+struct Theme: Identifiable, Equatable {
+    var emojis: [String]
+    var id: Int
+}
+
 class EmojiMemoryGame: ObservableObject {
     
     static let numberOfPairsOfCardsGlb = 8
-    static var emoji_themes: [[String]] = [[String]]()
+    static var emoji_themes: [Theme] = [Theme]()
     static var theme_colors: Array<Color> = Array<Color>()
     static var theme: Int = 0
     static var init_set = false
+    static var theme_counter: Int = 0
     
     static let emojis_theme1 = ["🚂", "🚀", "🚁", "🚜", "🚗", "🚄", "🛵", "🚅"]
     static let emojis_theme2 = ["🚗", "🚄", "🛵", "🚅", "🚂", "🚀", "🚁", "🚜"]
@@ -30,29 +36,36 @@ class EmojiMemoryGame: ObservableObject {
         add_theme(emojis: emojis_theme4, num_pairs: numberOfPairsOfCardsGlb, color: Color.orange)
         add_theme(emojis: emojis_theme5, num_pairs: numberOfPairsOfCardsGlb, color: Color.yellow)
         add_theme(emojis: emojis_theme6, num_pairs: numberOfPairsOfCardsGlb - 1, color: Color.brown)
-        let theme_count = emoji_themes.count
-        theme = Int.random(in: 0..<theme_count)
+    }
+    
+    static func get_emoji_themes() -> [Theme] {
+        return emoji_themes
+    }
+    
+    static func set_theme(id: Int) {
+        theme = id
     }
     
     static func add_theme(emojis: Array<String>, num_pairs: Int, color: Color) {
-        let diff = emojis.count - num_pairs
-        if diff >= 0, num_pairs >= 0 {
-            let emoji_arr = get_unique_random_array(size: emojis.count, diff: diff)
-            var emojis_loc = Array<String>()
-            
-            for index in 0..<num_pairs{
-                let emoji_index = emoji_arr[index]
-                emojis_loc.append(emojis[emoji_index])
-            }
-            
-            emoji_themes.append(emojis_loc)
-            theme_colors.append(color)
+
+        var emojis_loc = Theme(emojis: [], id: theme_counter)
+        let num_pairs = emojis.count
+        
+        emojis_loc.id = theme_counter
+        
+        for index in 0..<num_pairs {
+            emojis_loc.emojis.append(emojis[index])
         }
+        
+        emoji_themes.append(emojis_loc)
+        theme_colors.append(color)
+        
+        theme_counter = theme_counter + 1
         
     }
     
     static func create_card_content (index: Int) -> String {
-        return emoji_themes[theme][index]
+        return emoji_themes[theme].emojis[index]
     }
     
     static func createMemoryGame() -> MemoryGame<String> {
@@ -61,7 +74,7 @@ class EmojiMemoryGame: ObservableObject {
             EmojiMemoryGame.init_set = true
         }
         
-        return MemoryGame<String>(numberOfPairsOfCards: emoji_themes[theme].count, createCardContent: create_card_content)
+        return MemoryGame<String>(numberOfPairsOfCards: emoji_themes[theme].emojis.count, createCardContent: create_card_content)
     }
     
     static func get_unique_random_array(size: Int, diff: Int) -> Array<Int> {
@@ -95,9 +108,17 @@ class EmojiMemoryGame: ObservableObject {
     func new_game() {
         let theme_count = EmojiMemoryGame.emoji_themes.count
         EmojiMemoryGame.theme = Int.random(in: 0..<theme_count)
-        model.new_game(num_cards: EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme].count * 2) {
+        model.new_game(num_cards: EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme].emojis.count * 2) {
             index in
-            return EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme][index]
+            return EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme].emojis[index]
+        }
+    }
+    
+    func new_game_with_id(id: Int) {
+        EmojiMemoryGame.theme = id
+        model.new_game(num_cards: EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme].emojis.count * 2) {
+            index in
+            return EmojiMemoryGame.emoji_themes[EmojiMemoryGame.theme].emojis[index]
         }
     }
     
@@ -107,6 +128,10 @@ class EmojiMemoryGame: ObservableObject {
     
     func get_theme_color() -> Color {
         return EmojiMemoryGame.theme_colors[EmojiMemoryGame.theme]
+    }
+    
+    func get_color_of_theme_with(id: Int) -> Color {
+        return EmojiMemoryGame.theme_colors[id]
     }
     
     func get_score() -> Int {
